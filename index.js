622f91481f00e1c236c73837973952d09ea31ebb
Mauricio Silva Teixeira de Nobrega <mauricio.nobrega@gmail.com>
2017-09-18 15:23:49 -0300

    Init

---
+++ index.js
@@ -0,0 +1,15 @@
+let = templates = [];
+const json = require("./rules.json");
+const actives = json.filter(function(element) {
+    return element.active;
+});
+
+actives.filter(function(element) {
+    const mob = element.templateRules.find(function(rule) {
+        if (rule.name === 'mobile' && templates.indexOf(rule.template) === -1) {
+            templates.push(rule.template)
+        }
+    });
+});
+console.log(templates);
+console.log(templates.length);
