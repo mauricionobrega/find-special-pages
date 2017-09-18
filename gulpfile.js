818ecca3d530b00d86d9ee617ab99f31c2bfae24
Mauricio Silva Teixeira de Nobrega <mauricio.nobrega@gmail.com>
2017-09-18 17:28:02 -0300

    add timestamp

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,26 @@
 const path = process.env.CMS_PROD_LOCAL_PATH;
+const dist = './dist';
 const gulp = require('gulp');
-const cachebust = require('gulp-cache-refresh');
+const del = require('del');
+const replace = require('gulp-replace');
 const templates = require('./modules/templates')(path);
+const timestamp = new Date().getTime();
 
-// console.log('templates ', templates);
+console.log('templates ', templates);
+console.log(`UPDATE FILES WITH TIMESTAMP: ${timestamp}`);
 
-gulp.src(templates, {cwd: './'}).pipe(cachebust({
-  type: 'timestamp'
-})).on('error', function() {
-  console.log(arguments);
-}).pipe(gulp.dest('./dist'));
+gulp.task('clean', () => {
+  del([dist]);
+});
+
+gulp.task('bust', () => {
+  return gulp.src(templates, {cwd: './'}).pipe(replace(/(min.js|min.css)[^\/"&]/g, (match, p1, offset, string) => {
+    // See http://mdn.io/string.replace#Specifying_a_function_as_a_parameter
+    // https://www.npmjs.com/package/gulp-replace
+    return `${match}${timestamp}`;
+  })).on('error', () => {
+    console.log(arguments);
+  }).pipe(gulp.dest(dist));
+});
+
+gulp.task('default', ['clean', 'bust']);
