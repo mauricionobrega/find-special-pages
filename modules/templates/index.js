aa7ececc7e73f6467eb8c52dc0e1fe8400fcb106
Mauricio Silva Teixeira de Nobrega <mauricio.nobrega@gmail.com>
2017-09-18 17:50:13 -0300

    Update

--- modules/templates/index.js
+++ modules/templates/index.js
@@ -3,12 +3,12 @@ module.exports = (path) => {
 
   let templates = [];
   const json = require('./rules.json');
-  const actives = json.filter(function(element) {
+  const actives = json.filter((element) => {
     return element.active;
   });
 
-  actives.filter(function(element) {
-    const mob = element.templateRules.find(function(rule) {
+  actives.filter((element) => {
+    const mob = element.templateRules.find((rule) => {
       if (rule.name === 'mobile' && templates.indexOf(rule.template) === -1) {
         templates.push(`${path}/${rule.template}`);
       }
